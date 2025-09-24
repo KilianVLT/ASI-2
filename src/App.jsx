bc3e94947f608c0bd7cfb2813831ea4aae3d8103
KilianCPE <kilian.violet@cpe.fr>
2025-09-24 09:14:08 +0200

    useState count Over

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
 import { useState } from 'react'
+import { Button } from "react-bootstrap";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(props) {
+  const [title, setTitle] = useState(props.title);
+  const [count, setCount] = useState(0);
+
+  const handleChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1 onMouseOver={() => setCount(count + 1)}> this is my first React Component</h1>
+      <label htmlFor="titleInput">Title</label>
+      <input
+        type="text"
+        id="titleInput"
+        onChange={handleChangeTitle}
+        value={title}
+      />
+      <h3>{title}</h3>
+      <h3>Vous avez survolé le titre {count} fois</h3>
+    </div>
+  );
 }
-
-export default App
+export default App;
